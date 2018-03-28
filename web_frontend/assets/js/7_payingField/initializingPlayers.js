182c0f20dcd602ca872347abae6b937c56efdea8
Boucquet Louis <louis.boucquet@student.howest.be>
2018-03-28 17:31:01 +0200

    Added the 7_field javascript folder with srcipts

---
+++ web_frontend/assets/js/7_payingField/initializingPlayers.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// opponent
+let opponent = {
+  "type" : "opponent"
+  "heroName" : 'priest',
+  "heroWeapon" : true,
+  "weaponAttack" : ,
+  "weaponTurns" : 8,
+  "amountShield" : 10,
+  "lifepoints" : 30,
+  "manaCharge" : 1,
+  "manaLimit" : 11,
+  "arrayCardsInHand" : [1, 2, 3, 4],
+  "arrayCardsOnField" : []
+}
+
+// player
+let player = {
+  "type" : "player"
+  "heroName" : 'rogue',
+  "heroWeapon" : true,
+  "weaponAttack" : 10,
+  "weaponTurns" : 3,
+  "amountShield" : 0,
+  "lifepoints" : 30,
+  "manaCharge" : 8,
+  "manaLimit" : 8,
+  "arrayCardsInHand" : [],
+  "arrayCardsOnField" : []
+}
---
+++ web_frontend/assets/js/7_payingField/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init(){
+  // TODO: functis samen smijten
+    endTurn(true);
+    manaCystalOpponent(manaChargeOpponent, manaLimitOpponent);
+    manaCrystal(manaCharge, manaLimit);
+    heroOpponent(heroWeaponOpponent, weaponAttackOpponent, weaponTurnsOpponent, heroNameOpponent, amountShieldOpponent, lifepointsOpponent);
+    hero(heroWeapon, weaponAttack, weaponTurns, heroName, amountShield, lifepoints);
+    cardsInHandOpponent(arrayCardsInHandOpponent);
+    fetchCards();
+  // fetchPromise komt uit 4_fetchCardsFunctions
+  fetchPromise.then(function () {
+    // TODO: injectCards
+  }
+}
+
+function cardController(X, Y, original) {
+  if (Y < 800) {
+      // TODO: LOUIS hand.appendChild(original)
+  } else {
+    let out = document.querySelector(".cardsOnField");
+  }
+}

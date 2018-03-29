fec9902fe8608e97727ca62038d3b3b302d858a4
Unknown <louisboucquet@gmail.com>
2018-03-29 21:51:20 +0200

    Update
    aj over een kaart inj deck hangt ziej een grooter voorbeeld

--- web_frontend/assets/js/7_payingField/main.js
+++ web_frontend/assets/js/7_payingField/main.js
@@ -16,10 +16,12 @@ function init(){
     // fetchPromise komt uit 4_fetchCardsFunctions
     fetchPromise.then(function () {
         injectCards(player.arrayCardsInHand);
-        
+
         let elements = document.querySelectorAll(".cardHand")
         for (let i = 0; i < elements.length; i++) {
-            elements[i].addEventListener("mousedown", cardMousedown)
+            elements[i].addEventListener("mousedown", cardMousedown);
+            elements[i].addEventListener("mouseenter", cardOnmouseEnter);
+            elements[i].addEventListener("mouseleave", cardOnmouseLeave);
         }
     });
     // makeMovable shit

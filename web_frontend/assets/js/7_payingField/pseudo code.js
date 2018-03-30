bc6c8c3adfb793250f2dd216b989e010ad1cba7c
Unknown <Rutger.coopman@student.howest.be>
2018-03-30 10:38:23 +0200

    pseudo code voor logica playfield niet af!

---
+++ web_frontend/assets/js/7_payingField/pseudo code.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//
+// if button is push or times up change player, player
+//
+// if weapon is just, weapon is off offline(change picture) etc to power.toString()
+//
+// if weapon card has be summond, weapon weapon can be just till the turns are over. -1 every time when its been just
+//
+// if weapon, power of minion as summond then, do manacharge - mana of the weapon, power or minion
+//
+// if manacharge is kleiner dan weapon, power, minion then can not be just. nothing will happen
+//
+// if opponent is playing, player can not do any thing
+//
+// if lifepoints is 0. the game is played
+//
+// if a player as a taunt, only the taunt can be attacked
+//
+// if player attack with power then deal the damage that the power has
+//
+// if player attack with minion on hero deal the attack to the hero
+//
+// if player attack with minion on a minion deal the attackS to the other hes lifepoints
+//
+// if lifepoints of the minion <= 0 then it is dead and be removed from the field
+//
+// if player just minion with +1 att of def, add to ONE minion or all
+//
+// if player turn begin +1mana limit
+//
+// if player turn starts manacharge = manalimit
+//
+//
+//

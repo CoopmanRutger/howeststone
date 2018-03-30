780c4d326a60c5fd40cd945121c7d50058e742b2
Unknown <louisboucquet@gmail.com>
2018-03-30 17:25:07 +0200

    dynamisch deckbuilder One

--- web_frontend/assets/js/8_deckSelector.js
+++ web_frontend/assets/js/8_deckSelector.js
@@ -1,88 +1,78 @@
 "use strict";
-let arrayHeroes = ["mage", "priest"];
+
+let heroesAndDecks = [
+    {"name":"penis",
+"img":"blablabla",
+"decks":
+[{"name":"penis",
+"img":"blablabla"}]
+},
+{"name":"penis",
+"img":"blablabla",
+"decks":[{"name":"penis",
+"img":"blablabla"}]
+}
+];
+
 document.addEventListener("DOMContentLoaded", init);
-let arrayDecks = ["mage", "priest", "rouge"];
 
 function init() {
-    showHeroPic();
-    showHeroes();
-
+    injectHeroDecks();
 }
 
-function showHeroes() {
-    for (let j = 0; j < arrayHeroes.length; j++) {
-        let hero = document.createElement("article");
-        let heroName = document.createElement("h2");
-        let heroImg = document.createElement("div");
-        let heroList = document.createElement("li");
+function injectHeroDecks() {
+    for (let i = 0; i < heroesAndDecks.length; i++) {
+        console.log(heroesAndDecks[i]);
 
+        let hero = document.createElement("li");
+        hero.className = "hero";
 
-        let text = document.createTextNode(arrayHeroes[j].name);
+        let decks = document.createElement("ul");
+        decks.className = "decks";
 
-        heroList.appendChild(hero);
-        heroName.appendChild(text);
-
-        heroImg.style.backgroundImage = "url('" + arrayHeroes[j].img + "')";
-
-        document.querySelector(".heroes").appendChild(text);
-        for (let i = 0; i < arrayHeroes[i].decks.length; i++) {
-            let deck = document.createElement("article");
-            let deckName = document.createElement("h2");
-            let deckImg = document.createElement("div");
-            let text = document.createTextNode(arrayHeroes[i].deck.name);
-
-            deckName.appendChild(text);
-
-            deckImg.style.backgroundImage = "url('" + arrayHeroes[i].deck.img + "')";
-
-            document.querySelector(".decks").appendChild(text);
-
+        for (let j = 0; j < heroesAndDecks[i].decks.length; j++) {
+            console.log(heroesAndDecks[i].decks[i]);
+            let deck = document.createElement("li");
+            deck.appendChild(showDeck(heroesAndDecks[i].decks[j].name, heroesAndDecks[i].decks[j].img));
+            decks.appendChild(deck);
         }
+
+        hero.appendChild(showHero(heroesAndDecks[i].name, heroesAndDecks[i].img));
+        hero.appendChild(decks);
+
+        document.querySelector(".heroes").appendChild(hero)
     }
 }
 
-// function showHeroPic() {
-//     for (let j = 0; j <= arrayHeroes.length; j++) {
-//
-//         let element = document.createElement("section");
-//         let title = document.createElement("h2");
-//         document.querySelector(".hero").appendChild(element);
-//         element.style.backgroundImage = "url('images/hero_" + arrayHeroes[j] + ".png')";
-//         title.appendChild(document.createTextNode(arrayHeroes[j]));
-//
-//
-//         element.appendChild(title);
-//         element.style.height = 50 + "px";
-//         element.style.width = 50 + "px";
-//     }
-// }
+function showHero(name, img) {
+    let hero = document.createElement("article");
+    hero.className = "heroPreview"
+    let heroName = document.createElement("h2");
 
-function showDeck() {
-    let element = document.createElement("section");
-    let title = document.createElement("h3");
+    let text = document.createTextNode(name);
+    heroName.appendChild(text);
 
+    let heroImg = document.createElement("div");
+    heroImg.style.backgroundImage = "url('" + img + "')";
+
+    hero.appendChild(heroName);
+    hero.appendChild(heroImg);
+    return hero;
 }
 
-//
-// function showDeck() {
-//     let element = document.createElement("section");
-//     let title = document.createElement("h3");
-//
-//     for (let j = 0; j <= arrayDecks.length; j++) {
-//         if (arrayDecks[j] === arrayDecks[j]) {
-//             if (arrayDecks[j] === arrayDecks[0]) {
-//                 document.querySelector(".deck").appendChild(element);
-//             } else if (arrayDecks[j] === arrayDecks[1]) {
-//                 document.querySelector(".deck:nth-child(2)").appendChild(element);
-//             } else if (arrayDecks[j] === arrayDecks[2]) {
-//                 document.querySelector(".deck:last-child").appendChild(element);
-//             }
-//             element.style.backgroundImage = "url('images/hero_" + arrayDecks[j] + ".png')";
-//             title.appendChild(document.createTextNode(arrayDecks[j]));
-//         }
-//
-//         element.appendChild(title);
-//         element.style.height = 50 + "px";
-//         element.style.width = 50 + "px";
-//     }
-// }
+function showDeck(name, img) {
+    let element = document.createElement("article");
+    element.className = "deck";
+
+    let title = document.createElement("h3");
+    element.appendChild(title);
+
+    let text = document.createTextNode(name);
+    title.appendChild(text)
+
+    let heroImg = document.createElement("div");
+    heroImg.style.backgroundImage = "url('" + img + "')";
+    element.appendChild(heroImg);
+
+    return element;
+}

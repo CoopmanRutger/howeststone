69492ef7adf854aa285e1e3a5ae551822b926c66
Unknown <louisboucquet@gmail.com>
2018-04-19 16:03:11 +0200

    deleted our tests

--- java_backend/src/main/java/api/Routes.java
+++ java_backend/src/main/java/api/Routes.java
@@ -2,18 +2,17 @@ package api;
 
 import io.javalin.Context;
 import io.javalin.Javalin;
-import model.PlayingCard;
 
 class Routes {
 
     Routes(final Javalin server) {
         server.get("/", this::handleRoot);
-        server.get("/card/", this::getCardJson);
+//        server.get("/card/", this::getCardJson);
     }
 
-    private void getCardJson(Context context) {
-        context.json(new PlayingCard("bert",1));
-    }
+//    private void getCardJson(Context context) {
+//        context.json(new PlayingCard("bert",1));
+//    }
 
     private void handleRoot(final Context context) {
         context.result("Hey you");

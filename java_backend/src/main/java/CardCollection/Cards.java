250d8605ee5e20b24500df25de33712c6fb78315
The Great mister boucqsky <louisboucquet@gmail.com>
2018-04-24 10:18:58 +0200

    Auto stash before revert of "fixed bug"

--- java_backend/src/main/java/CardCollection/Cards.java
+++ java_backend/src/main/java/CardCollection/Cards.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Cards {
-    private Set<String> cards;
+    private Set<Cards.Card> cards;
 
     public Cards(Set cards) {
         this.cards = cards;
@@ -14,20 +14,20 @@ public class Cards {
         cards = new HashSet<>();
     }
 
-//    public void add(Cards.CardMinion card) {
-//        cards.add(card);
-//    }
-//
-//    public void add(Cards.CardSpell card) {
-//        cards.add(card);
-//    }
-//
-//    public void add(Cards.CardWeapon card) {
-//        cards.add(card);
-//    }
+    public void add(Cards.CardMinion card) {
+        cards.add(card);
+    }
 
-    public String get(){
-        String cardToReturn = cards.iterator().next();
+    public void add(Cards.CardSpell card) {
+        cards.add(card);
+    }
+
+    public void add(Cards.CardWeapon card) {
+        cards.add(card);
+    }
+
+    public Cards.Card get(){
+        Cards.Card cardToReturn = cards.iterator().next();
         cards.remove(cardToReturn);
         return cardToReturn;
     }

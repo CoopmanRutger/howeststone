ce53ab635836c3ef3f4224d469d198e774f72aa0
Rutger Coopman <Rutger.coopman@student.howest.be>
2018-04-24 15:18:01 +0200

    howestStone.ncp en sql aangepast

---
+++ java_backend/src/main/java/SQLcontoller/SqlDatabase.java
@@ -0,0 +1,28 @@
+package SQLcontoller;
+
+import java.sql.*;
+
+public class SqlDatabase {
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Failed to load database driver, shutting down ...");
+            System.exit(-1);
+        }
+    }
+
+    private final String url, user, password;
+
+    public SqlDatabase(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+}

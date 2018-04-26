65b8e8be256af16e8bca5006001fc840ab5c6dc0
Ninooooo <nino.florizoone@student.howest.be>
2018-04-26 12:40:32 +0200

    finished class Template
    getters setters done
    constructor done

--- java_backend/src/main/java/templates/Template.java
+++ java_backend/src/main/java/templates/Template.java
@@ -5,7 +5,18 @@ import heroes.Hero;
 
 public class Template {
     private Hero hero;
-    private Deck deck1;
+    private Deck deck;
 
+    public Template(Hero hero, Deck deck) {
+        this.hero = hero;
+        this.deck = deck;
+    }
 
+    public Hero getHero() {
+        return hero;
+    }
+
+    public Deck getDeck1() {
+        return deck;
+    }
 }

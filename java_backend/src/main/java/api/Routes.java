351382400c123f54a114a4e44513f68d8fb43fdb
Rutger Coopman <Rutger.coopman@student.howest.be>
2018-04-26 12:39:42 +0200

    laatste versie

--- java_backend/src/main/java/api/Routes.java
+++ java_backend/src/main/java/api/Routes.java
@@ -8,24 +8,27 @@ class Routes {
 
     Routes(final Javalin server) {
         server.get("/", this::handleRoot);
-        server.post("/minions", this::minions);
+        server.get("/minions", this::minions);
 //        server.get("/card/", this::getCardJson);
     }
 
-//    private void getCardJson(Context context) {
-//        context.json(new PlayingCard("bert",1));
-//    }
+    private void minions(Context context) {
+        InitDeckBuilderLvl2 deck = new InitDeckBuilderLvl2();
+        context.json(deck.selectMinion());
+    }
 
     private void handleRoot(final Context context) {
         context.result("Hey you");
     }
 
-    private void minions() {
-        InitDeckBuilderLvl2 lol = new InitDeckBuilderLvl2();
-        lol.selectMinion();
-        return ;
-
-
-    }
-
 }
+
+//    private void getCardJson(Context context) {
+//        context.json(new PlayingCard("bert",1));
+//    }
+
+//        server.post("/minions", context -> {
+////            context.status(201);
+//            InitDeckBuilderLvl2 deck = new InitDeckBuilderLvl2();
+//            context.json(deck.selectMinion());
+//        });
--- java_backend/src/main/java/cards/CardMinion.java
+++ java_backend/src/main/java/cards/CardMinion.java
@@ -4,16 +4,14 @@ public class CardMinion extends Card{
     private int attack;
     private int health;
     private String race;
-    private String imgBack;
     private String mechanicsName;
 
 
-    public CardMinion(String cardId, String name, String type, int mana, String heroType, String description, String img, int attack, int health, String race, String imgBack, String mechanicsName) {
+    public CardMinion(String cardId, String name, String type, int mana, String heroType, String description, String img, int attack, int health, String race, String mechanicsName) {
         super(cardId, name, type, mana, heroType, description, img);
         this.attack = attack;
         this.health = health;
         this.race = race;
-        this.imgBack = imgBack;
         this.mechanicsName = mechanicsName;
     }
 
@@ -27,10 +25,6 @@ public class CardMinion extends Card{
 
     public String getRace() {
         return race;
-    }
-
-    public String getImgBack() {
-        return imgBack;
     }
 
     public String getMechanicsName() {

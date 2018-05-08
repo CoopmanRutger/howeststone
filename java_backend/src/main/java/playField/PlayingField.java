30d8f6f6d2f0466da9cbb7f63bc2ec60ebf55d58
The Great mister boucqsky <louisboucquet@gmail.com>
2018-05-08 11:27:11 +0200

    updated playingfiel class

--- java_backend/src/main/java/playField/PlayingField.java
+++ java_backend/src/main/java/playField/PlayingField.java
@@ -5,17 +5,27 @@ import player.Player;
 
 public class PlayingField {
     Player players[] = new Player[2];
-    Bot opponent;
-    Player player;
     private int turn = 1;
     private final int maxMana = 10;
     private short index = 0;
-  //*  Player CurrentPlayer;
+    // Player CurrentPlayer;
 
-    
-    public Player selectNextPlayer() {
-        index++;
-        index %= 2;
-        return players[index];
+    private void incerement(){
+        if (index == 1) {
+            turn++;
+            index = 0;
+        } else if (index == 0) {
+            index++;
+        } else {
+//            't is nie gelukt
+        }
+    }
+
+    public int getMana(){
+        if (turn < maxMana){
+            return turn;
+        } else {
+            return maxMana;
+        }
     }
 }

74ef6fddfd60d797ff534bdd654885709fa04d8f
Bert <bert.vervust@student.howest.be>
2018-05-17 15:55:59 +0200

    Auto stash before merge of "backend" and "origin/backend"

---
+++ java_backend/src/test/java/cards/CardWeaponTest.java
@@ -0,0 +1,22 @@
+package cards;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardWeaponTest {
+
+    private CardWeapon test;
+
+    @Before
+    public  void before(){
+        test = new CardWeapon("id01","name", "blue", 25, "blueGuy", "i am a faggot", "muh image",41, 20);
+    }
+
+    @Test
+    public void defaultAttack(){
+        assertEquals(15, test.defaultAttack());
+    }
+
+}
---
+++ java_backend/src/test/java/heroes/HeroTest.java
@@ -0,0 +1,33 @@
+package heroes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    private HeroPower heroPower;
+    private Hero hero;
+
+    @Before
+    public void before(){
+        heroPower = new HeroPower(5, 5, true);
+        Hero test = new Hero("Jaina", "muh img", heroPower);
+
+
+    }
+    @Test
+    public void takeDamage(int damage){
+ {
+
+        hero.takeDamage(3);
+        assertEquals(27, hero.getLifePoints());
+
+        hero.takeDamage(4);
+        assertEquals(23, hero.getLifePoints());
+
+        hero.takeDamage(25);
+        assertEquals(-2,hero.getLifePoints());
+    }
+}

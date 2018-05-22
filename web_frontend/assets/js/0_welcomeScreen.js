fdd8fa060089cb7c39d7f59169bd53738e6f73ef
Bert <bert.vervust@student.howest.be>
2018-05-22 18:37:29 +0200

    Auto stash before merge of "backend" and "origin/backend"

--- web_frontend/assets/js/0_welcomeScreen.js
+++ web_frontend/assets/js/0_welcomeScreen.js
@@ -1,13 +1,13 @@
-"use strict"
+"use strict";
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-    document.querySelector('a').addEventListener('click', nextScreen);;
+    document.querySelector('a').addEventListener('click', nextScreen);
 }
 
 function nextScreen(e) {
     e.preventDefault();
-    window.location.href = "1_deckbuildOrPlay.html"
+    window.location.href = "1_deckbuildOrPlay.html";
 
-    return false; ;
+    return false;
 }
